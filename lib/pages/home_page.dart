40f54925e488a0f99b7398f6467650b2d9db733e
Igbozulike Basil <IgbozulikeBasil@NONSOs-MacBook-Pro.local>
2022-09-23 23:36:25 +0100

    added things

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final formkey = GlobalKey<FormState>();
+  final snackbar = const SnackBar(
+    content: Text('Submitting form'),
+    duration: Duration(seconds: 5),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+          child: Column(
+            children: [
+              Form(
+                  key: formkey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: height * 0.04,
+                      ),
+                      const Text(
+                        'Sign up ',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                            color: Color(0xFF363f93)),
+                      ),
+                      const Text(
+                        'Get ready to eat !',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                            color: Color(0xFF363f93)),
+                      ),
+                      SizedBox(
+                        height: height * 0.05,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Enter your name',
+                          suffixIcon: Icon(Icons.person),
+                        ),
+                        validator: ((value) {
+                          if (value!.isEmpty ||
+                              !RegExp(r'^[a-z A-Z] +$').hasMatch(value)) {
+                            return 'Enter valid name';
+                          } else {
+                            return null;
+                          }
+                        }),
+                      ),
+                      SizedBox(
+                        height: height * 0.05,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'Enter your email',
+                            suffixIcon: Icon(Icons.email)),
+                        validator: ((value) {
+                          if (value!.isEmpty ||
+                              !RegExp(r'^[a-z A-Z] +$').hasMatch(value)) {
+                            return 'Enter valid email';
+                          } else {
+                            return null;
+                          }
+                        }),
+                      ),
+                      SizedBox(
+                        height: height * 0.05,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Enter your password',
+                          suffixIcon: Icon(Icons.lock),
+                        ),
+                        obscureText: true,
+                        validator: ((value) {
+                          if (value!.isEmpty ||
+                              !RegExp(r'^[a-z A-Z] +$').hasMatch(value)) {
+                            return 'Enter valid password';
+                          } else {
+                            return null;
+                          }
+                        }),
+                      ),
+                      SizedBox(
+                        height: height * 0.05,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Sign Up',
+                            style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF363f93)),
+                          ),
+                          FloatingActionButton(
+                            onPressed: () {
+                              if (formkey.currentState!.validate()) {
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(snackbar);
+                              }
+                            },
+                            backgroundColor: const Color(0xFF363f93),
+                            child: const Icon(Icons.forward),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: height * 0.1,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text('Sign In',
+                              style: TextStyle(color: Color(0xFF363f93))),
+                          Text(
+                            'Forgot Password',
+                            style: TextStyle(color: Color(0xFF363f93)),
+                          )
+                        ],
+                      )
+                    ],
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
